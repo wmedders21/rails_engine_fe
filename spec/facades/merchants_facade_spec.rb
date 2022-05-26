1e101b5c21e8095a9c4be9453f54b4093bf428eb
Will Medders <wmedders@gmail.com>
2022-05-25 21:57:16 -0400

    Feat: merchants facade #all_merchants

---
+++ spec/facades/merchants_facade_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'merchants facade', :vcr do
+  describe 'class methods' do
+    it '#all_merchants' do
+      expect(MerchantsFacade.all_merchants).to be_a(Array)
+      expect(MerchantsFacade.all_merchants.first).to be_a(Merchant)
+    end
+  end
+end
