72092840ec2edf21da981ed491ee93ffef45e3a3
Will Medders <wmedders@gmail.com>
2022-05-25 22:01:04 -0400

    Feat: merchants facade #merchant_items

--- spec/facades/merchants_facade_spec.rb
+++ spec/facades/merchants_facade_spec.rb
@@ -10,5 +10,10 @@ RSpec.describe 'merchants facade', :vcr do
     it '#merchant', :vcr do
       expect(MerchantsFacade.merchant(1)).to be_a(Merchant)
     end
+
+    it '#merchant_items', :vcr do
+      expect(MerchantsFacade.merchant_items(1)).to be_a(Array)
+      expect(MerchantsFacade.merchant_items(1).first).to be_a(Item)
+    end
   end
 end
